6e89f38a1df936a718d2de752c6ce04692845446
Vikram Belde <symmetriccurve@gmail.com>
2018-10-07 15:08:39 -0500

    Syntax fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-export default findAllMatches(sentences, inputSearchSting,key) {
+export default function(sentences, inputSearchSting, key) {
   var searchString = inputSearchSting.split(' ')// ["test1","test2", . . .]
-  
+
   var regExString = '^'
   //Building RegEx String based on inputSearchSting
   searchString.forEach(eachWord => {
@@ -13,7 +13,7 @@ export default findAllMatches(sentences, inputSearchSting,key) {
   var matches = sentences.filter(eachSentence => {
     //console.log(eachSentence.match(regEx))
     if(key) return eachSentence[key].toLowerCase().match(regEx) != null
-    return eachSentence.toLowerCase()..match(regEx) != null
+    return eachSentence.toLowerCase().match(regEx) != null
   })
   return matches
 }
